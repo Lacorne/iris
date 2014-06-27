c8d8fe5a07255aaa3f1d975064c16f2d6d7894d8
llacorne <externe.ludovic.lacorne@cellfishmedia.fr>
2014-06-27 13:51:44 +0200

    update to D7.28 + update modules contrib

--- sites/all/libraries/ckeditor/config.js
+++ sites/all/libraries/ckeditor/config.js
@@ -1,30 +1,10 @@
-﻿/*
-Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
 
-CKEDITOR.editorConfig = function( config )
-{
+CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
-	 config.language = 'fr';
+	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-
- 
-      
-
-   config.toolbar = 'MyToolbar_defaut';
-    config.toolbar_MyToolbar_defaut =
-    [    
-    ['Bold','Italic','Underline','Strike','-'],
-    config.toolbar = 'MyToolbar_user';
-
-    ];
-    config.toolbar_MyToolbar_user =
-    [    
-    ['Bold','Italic','Underline','Strike','-'],
-    ['NumberedList','BulletedList','-','Outdent','Indent'],
-    ['JustifyLeft','JustifyCenter'],
-    ];
-
-
 };
